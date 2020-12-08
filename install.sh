327e64797c617094400b39e1569f8d9ad9e19b5d
joaqo <joaquin.alori@gmail.com>
2020-12-08 16:28:09 +0000

    Add vim's PlugInstall to install script

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ ln -s -f .dotfiles/.gitconfig .
 ln -s -f .dotfiles/.inputrc .
 ln -s -f .dotfiles/.tmux.conf .
 ln -s -f .dotfiles/.vimrc .
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim -c 'PlugInstall' # -c 'qa!' # Not sure if this waits for the previous command so I'm commenting it out
