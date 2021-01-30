a89586e1a787ebb91f3cbe8e4ee2c2f91d209fa1
joaqo <joaquin.alori@gmail.com>
2021-01-30 12:14:14 -0300

    Improve install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 # Run me from your home folder
 
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh --directory-prefix=/usr/local/etc/bash_completion.d/
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash --directory-prefix=/usr/local/etc/bash_completion.d/
 ln -s -f .dotfiles/.bashrc .
 ln -s -f .dotfiles/.profile .
 ln -s -f .dotfiles/.gitconfig .
 ln -s -f .dotfiles/.inputrc .
 ln -s -f .dotfiles/.tmux.conf .
 ln -s -f .dotfiles/.vimrc .
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim -c 'PlugInstall' # -c 'qa!' # Not sure if this waits for the previous command so I'm commenting it out
+vim -c 'PlugInstall' -c 'qa!'
