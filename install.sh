7d173506b6210fd003e5e52c61fcf1ac1757fcf5
joaqo <joaquin.alori@gmail.com>
2022-02-24 04:38:19 -0300

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 # Run me from your home folder
 
 mkdir -p ~/.bin
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh --directory-prefix="$HOME/.bin/"
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash --directory-prefix="$HOME/.bin/"
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > "$HOME/.bin/git-prompt.sh"
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > "$HOME/.bin/git-completion.bash"
 ln -s -f .dotfiles/.bashrc .
 ln -s -f .dotfiles/.profile .
 ln -s -f .dotfiles/.gitconfig .
@@ -10,6 +10,8 @@ ln -s -f .dotfiles/.inputrc .
 ln -s -f .dotfiles/.tmux.conf .
 ln -s -f .dotfiles/.vimrc .
 ln -s -f .dotfiles/.vimrc_coc .
+mkdir -p .vim
+ln -s -f $HOME/.dotfiles/.vim/coc-settings.json .vim/coc-settings.json
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
