a231b30c9a89181008e49f4c636898e19bba259f
joaqo <joaquin.alori@gmail.com>
2023-05-02 10:31:40 -0300

    Make panes easier to see when using multiple of them in vim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -12,9 +12,10 @@ return {
     config = function(lazy_plugin, opts)
         vim.o.background = "dark"
         vim.cmd.colorscheme("gruvbox")
-        vim.cmd("hi Normal ctermbg=black | hi StatusLine ctermbg=red ctermfg=black | hi SignColumn ctermbg=black")
+        vim.cmd("hi Normal ctermbg=black | hi StatusLine ctermfg=darkgrey | hi SignColumn ctermbg=black")
+        -- Set searching colors
         vim.cmd("hi Search ctermfg=white ctermbg=darkgrey | hi IncSearch ctermbg=red ctermfg=white | hi CurSearch ctermbg=red ctermfg=white")
-        -- For `gitsigns` plugin, here for all color names: https://github.com/lewis6991/gitsigns.nvim/blob/f388995990aba04cfdc7c3ab870c33e280601109/doc/gitsigns.txt#L937
+        -- Set `gitsigns` plugin colors. Here for all color names: https://github.com/lewis6991/gitsigns.nvim/blob/f388995990aba04cfdc7c3ab870c33e280601109/doc/gitsigns.txt#L937
         vim.cmd("hi GitSignsAdd ctermbg=black ctermfg=darkcyan | hi GitSignsChange ctermbg=black ctermfg=green | hi GitSignsDelete ctermbg=black ctermfg=red | hi GitSignsChangedelete ctermbg=black ctermfg=brown | hi GitSignsTopdelete ctermbg=black ctermfg=red | hi GitSignsUntracked ctermbg=black ctermfg=lightgray")
     end
   },
