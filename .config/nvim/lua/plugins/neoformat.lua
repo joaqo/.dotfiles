200623bc1d004cd98136ddf301e4dfdccd4ff615
joaqo <joaquin.alori@gmail.com>
2024-06-23 04:32:38 -0300

    Install neoformat for nvim

---
+++ .config/nvim/lua/plugins/neoformat.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "sbdchd/neoformat",
+    cmd = "Neoformat",
+    keys = {
+      { "<leader>F", ":Neoformat<cr>", desc = "Run formatter", mode = "n" },
+      { "<leader>F", ":Neoformat<cr>", desc = "Run formatter", mode = "v" },
+    },
+  }
+}
