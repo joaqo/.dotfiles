2caf7534491726b075c6a025a7b70c497d8dd844
joaqo <joaquin.alori@gmail.com>
2026-01-25 14:04:16 -0300

    Update nvim packages with Lazy

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,54 +6,47 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    config = function(lazy_plugin, opts)
-      -- Mappings.
-      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+    config = function()
+      -- Diagnostic mappings
       local opts = { noremap=true, silent=true }
       vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
       vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
       vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-      vim.keymap.set('n', '<leader>Q', vim.diagnostic.setqflist, opts)  -- Open local warnings/errors in quickfix list
-      -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)  -- Open local warnings/errors in location list list
+      vim.keymap.set('n', '<leader>Q', vim.diagnostic.setqflist, opts)
 
-      -- Use an on_attach function to only map the following keys
-      -- after the language server attaches to the current buffer
-      local on_attach = function(client, bufnr)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+      -- LSP keymaps on attach
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local bufnr = args.buf
+          vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-        local bufopts = { noremap=true, silent=true, buffer=bufnr }
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-        vim.keymap.set('n', '<leader>wl', function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, bufopts)
-        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-        vim.keymap.set('n', '<leader>gf', function() vim.lsp.buf.format { async = true } end, bufopts)
-      end
+          local bufopts = { noremap=true, silent=true, buffer=bufnr }
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+          vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+          vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+          vim.keymap.set('n', '<leader>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, bufopts)
+          vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+          vim.keymap.set('n', '<leader>gf', function() vim.lsp.buf.format { async = true } end, bufopts)
+        end
+      })
 
-      require('lspconfig')['pyright'].setup{
-        on_attach = on_attach
-      }
-      require('lspconfig')['ts_ls'].setup{
-        on_attach = on_attach,
-        root_dir = function(fname)
-          local util = require('lspconfig').util
-          -- Prefer pnpm workspace root, then fall back to tsconfig, then git
-          return util.root_pattern('pnpm-workspace.yaml')(fname)
-            or util.root_pattern('tsconfig.json', 'jsconfig.json')(fname)
-            or util.find_git_ancestor(fname)
-        end,
-      }
-      require('lspconfig')['tailwindcss'].setup{
-        on_attach = on_attach
-      }
-      require('lspconfig')['eslint'].setup{
-        on_attach = on_attach
-      }
+      -- Server configs
+      vim.lsp.config('pyright', {})
+
+      vim.lsp.config('ts_ls', {
+        root_markers = { 'pnpm-workspace.yaml', 'tsconfig.json', 'jsconfig.json', '.git' },
+      })
+
+      vim.lsp.config('tailwindcss', {})
+
+      vim.lsp.config('eslint', {})
+
+      -- Enable servers
+      vim.lsp.enable({ 'pyright', 'ts_ls', 'tailwindcss', 'eslint' })
     end
   }
 }
