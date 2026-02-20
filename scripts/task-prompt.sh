3014d57abcfd8598b7f56b2854a8fb2023a1c627
joaqo <joaquin.alori@gmail.com>
2026-02-19 21:50:47 -0300

    fix(task-prompt): pass input as osascript arg to handle quotes

--- scripts/task-prompt.sh
+++ scripts/task-prompt.sh
@@ -7,25 +7,28 @@ if [ -z "$INPUT" ]; then
     exit 0
 fi
 
-osascript <<ITERM
-set itermWasRunning to application "iTerm" is running
+osascript - "$INPUT" <<'ITERM'
+on run argv
+    set inputText to item 1 of argv
+    set itermWasRunning to application "iTerm" is running
 
-tell application "iTerm"
-    if not itermWasRunning then
-        launch
-        delay 0.5
-        tell current session of current window
-            write text "cd ~/mellow && company task run " & quoted form of "$INPUT"
-        end tell
-    else
-        tell current window
-            set originalTab to current tab
-            set newTab to (create tab with default profile)
-            tell current session of newTab
-                write text "cd ~/mellow && company task run " & quoted form of "$INPUT"
+    tell application "iTerm"
+        if not itermWasRunning then
+            launch
+            delay 0.5
+            tell current session of current window
+                write text "cd ~/mellow && company task run " & quoted form of inputText
             end tell
-            select originalTab
-        end tell
-    end if
-end tell
+        else
+            tell current window
+                set originalTab to current tab
+                set newTab to (create tab with default profile)
+                tell current session of newTab
+                    write text "cd ~/mellow && company task run " & quoted form of inputText
+                end tell
+                select originalTab
+            end tell
+        end if
+    end tell
+end run
 ITERM
